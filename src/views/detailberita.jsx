0d2461f20ca727d917f21a56657cdcca3c5df5b8
mhmmdsyahputra <putra.cokers@gmail.com>
2023-01-21 11:10:04 +0700

    add feature coment in detail news

--- src/views/detailberita.jsx
+++ src/views/detailberita.jsx
@@ -16,6 +16,11 @@ const Detailberita = () => {
 
   const [berita, setBerita] = useState()
 
+  const [namaakun, setNamaAkun] = useState('')
+  const [comentar, setComentar] = useState('')
+
+
+
   const getDayNews = (data) => {
     const dateString = data
     const [year, month, day] = dateString.split("-");
@@ -24,17 +29,17 @@ const Detailberita = () => {
 
     if (dayOfWeek == 0) {
       return 'Minggu'
-    }else if(dayOfWeek == 1){
+    } else if (dayOfWeek == 1) {
       return 'Senin'
-    }else if(dayOfWeek == 2){
+    } else if (dayOfWeek == 2) {
       return 'Selasa'
-    }else if(dayOfWeek == 3){
+    } else if (dayOfWeek == 3) {
       return 'Rabu'
-    }else if(dayOfWeek == 4){
+    } else if (dayOfWeek == 4) {
       return 'Kamis'
-    }else if(dayOfWeek == 5){
+    } else if (dayOfWeek == 5) {
       return 'Jumat'
-    }else if(dayOfWeek == 6){
+    } else if (dayOfWeek == 6) {
       return 'Sabtu'
     }
 
@@ -42,16 +47,37 @@ const Detailberita = () => {
 
   useEffect(() => {
     document.title = "Detail"
-  },[]);
+  }, []);
 
-  useEffect(() => {
-    // MENGAMBIL DATA BERITA BERDASARKAN ID YG TERDAPAT DI URL TADI 
+  const kirimComent = (e) => {
+    e.preventDefault()
+    axios.
+      post(API_URL + `news/${id}/komentar`, {
+        nama: namaakun,
+        isiKomentar: comentar,
+      })
+    
+    setNamaAkun('')
+    setComentar('')
+  }
+
+  // MENGAMBIL DATA BERITA BERDASARKAN ID YG TERDAPAT DI URL TADI 
+  const GetBerita = () => {
     axios.
       get(API_URL + `news/${id}`)
       .then((res) => {
         const news = res.data;
         setBerita(news);
       })
+  }
+
+  useEffect(() => {
+    GetBerita() 
+  }, [kirimComent])
+  
+
+  useEffect(() => {
+    GetBerita()
   }, [])
 
   // JIKA TERDAPAT PERUBAHAN PADA STATE ID MAKA AMBIL ULANG BERDASARKAN ID YG DI SET ULANG TADI
@@ -85,7 +111,7 @@ const Detailberita = () => {
                     {
 
                     }
-                    {data.kategori} | {getDayNews(data.tglCreate.slice(0, 10)) } | {data.tglCreate.slice(0, 10)} | {data.tglCreate.slice(12, 19)}
+                    {data.kategori} | {getDayNews(data.tglCreate.slice(0, 10))} | {data.tglCreate.slice(0, 10)} | {data.tglCreate.slice(12, 19)}
                     <br />
                     <i className="fa-solid fa-eye"></i> {data.tayang}x Dilihat
                   </div>
@@ -98,6 +124,55 @@ const Detailberita = () => {
                 </div>
               ))
             }
+
+            <hr />
+
+            <div className="diskusi">
+              <h3 className='mb-3'>Berikan Comentar</h3>
+
+              <div className="col-md-5">
+
+                <form onSubmit={kirimComent}>
+                  <div className="mb-3">
+                    <label className="form-label">Nama</label>
+                    <input type="text" className="form-control" value={namaakun} onChange={e => { setNamaAkun(e.target.value) }} />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Pesan</label>
+                    <textarea className="form-control" value={comentar} onChange={e => { setComentar(e.target.value) }} rows="3"></textarea>
+                  </div>
+                  <div className="mb-3">
+                    <button className='btn btn-success'>Submit</button>
+                  </div>
+                </form>
+
+              </div>
+
+              <div className="allComent">
+                <h4> Isi Diskusi</h4>
+                <div className="row">
+                  {
+                    berita && berita.map((databerita, indexberita) => (
+                      databerita.komentar && databerita.komentar.reverse().map((datacoment, indexcoment) => (
+                        <div className="col-md-12 p-4 my-3" style={{ background: '#cfcfcf', borderTopRightRadius: '50px', borderBottomLeftRadius:'50px',borderBottomRightRadius:'50px' }}>
+                          <div className="nameakun fw-bold">
+                            {datacoment.nama}
+                          </div>
+                          <div className="isiComent">
+                            {datacoment.isiKomentar}
+                          </div>
+                          <div className="time text-end">
+                            {datacoment.tglKomentar.slice(0,21)}
+                          </div>
+
+                        </div>
+                      ))
+                    ))
+                  }
+
+                </div>
+              </div>
+            </div>
 
           </div>
 
